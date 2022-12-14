bc3c560fbaaf3ce4113792746f35e951fda98a26
Ewertonalex <ewertonlgk@gmail.com>
2022-12-14 15:23:13 -0300

    readme

--- cypress/e2e/home.cy.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="cypress" />
-
-describe('home page', () => {
-  it('App deve estar online', () => {
-    //cy.viewport(1440, 900)
-    cy.visit('https://buger-eats.vercel.app')
-    cy.get('#page-home main h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats')
-  })
-})
