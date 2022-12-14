0b0e16721fd9f51bbfe2549da5195268843b3075
Ewertonalex <ewertonlgk@gmail.com>
2022-12-14 14:39:19 -0300

    readme

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,20 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  
   e2e: {
+    //definir url de início
+    baseUrl: 'https://buger-eats-qa.vercel.app',
+
+    //conectando projeto ao dashboard do cypress:
+    projectId: "bx2eks",
+
+     //definir resolução da janela
+    viewportWidth: 1440,
+    viewportHeight: 900,
+    
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    projectId: "sw5cgy",
-    viewportWidth: 1440,
-    viewportHeight: 900,
-    baseUrl:'https://buger-eats-qa.vercel.app'
   },
 });
