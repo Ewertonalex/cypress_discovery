6a0cb02c54793bdda11d59a5aa64ed840d423d11
Ewertonalex <ewertonlgk@gmail.com>
2022-12-14 14:15:13 -0300

    readme

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,13 @@
-module.exports = {
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    projectId: "sw5cgy"
+    projectId: "sw5cgy",
     viewportWidth: 1440,
     viewportHeight: 900,
     baseUrl:'https://buger-eats-qa.vercel.app'
-    
   },
-};
+});
